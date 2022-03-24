a6e0f5a1473d397a0cdf416473e438cdac40e489
cristianllambro <llambro.cristian1996@gmail.comm>
2022-03-24 18:27:40 +0100

    add select on header

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -1,7 +1,15 @@
 <template>
-    <div class="header bg-secondary bg-gradient">
-        
-        icona
+    <div class="header bg-secondary bg-gradient d-flex justify-content-between align-items-center">
+        <p class="ps-3 text-uppercase">icona</p>
+        <div class="select-fix pe-3">
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Seleziona Genere</option>
+                <option value="1">Rock</option>
+                <option value="2">Pop</option>
+                <option value="3">Jazz</option>
+                <option value="3">Metal</option>
+            </select>
+        </div>
     </div>
 </template>
 
@@ -16,5 +24,9 @@ export default {
 .header{
     width: 100%;
     height: 70px;
+
+    .form-select{
+        width: 200px;
+    }
 }
 </style>
